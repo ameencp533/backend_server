fbffe8b11a036fcc5670f7fd6ce5f76368399c0f
ameencp <73467335+ameencp@users.noreply.github.com>
2023-08-11 15:53:44 +0530

    commiting

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,8 @@
 # api/urls.py
 from django.urls import path
-from .views import configure_device_view
+from .views import configure_device_view, data_collector_view
 
 urlpatterns = [
     path('configure-device/', configure_device_view, name='configure-device'),
+    path('data-collector/', data_collector_view, name='configure-device'),
 ]
--- api/views.py
+++ api/views.py
@@ -19,6 +19,35 @@ def make_conncn(device,config):
     except Exception as msg:
         return (str(msg),False)
 
+def collector(device,command):
+    
+    try:
+        with ConnectHandler(**device) as ssh:
+            result=ssh.send_command(command[0])
+            #result=result.replace('\n','<br>')
+            return (result,True)
+    except Exception as msg:
+        return (str(msg),False)
+
+def collect_data(payload):
+    iplist=payload['ipAddresses']
+    out_dic={}
+    for ip in iplist:
+        dev = {
+            'host': ip,
+            'username':payload['username'],
+            'password':payload['password'],
+            'device_type':payload['deviceType'],
+            'secret': payload['deviceType']
+            }
+        result=collector(dev,payload['configCommand'])
+        out_dic[ip]={
+            'status':result[1],
+            'output':result[0]
+        }
+    return out_dic
+
+
 
 
 
@@ -39,12 +68,29 @@ def configure_device(payload):
             'config_log':result[0]
         }
     return out_dic
+
+
+
 @csrf_exempt
 def configure_device_view(request):
     if request.method == 'POST':
         try:
             payload = json.loads(request.body)
             result=configure_device(payload)  # Call your function here
+            return JsonResponse(result)
+        except json.JSONDecodeError:
+            return JsonResponse({'error': 'Invalid JSON payload.'}, status=400)
+    return JsonResponse({'error': 'Only POST requests are allowed.'}, status=405)
+
+
+
+
+@csrf_exempt
+def data_collector_view(request):
+    if request.method == 'POST':
+        try:
+            payload = json.loads(request.body)
+            result=collect_data(payload)  # Call your function here
             return JsonResponse(result)
         except json.JSONDecodeError:
             return JsonResponse({'error': 'Invalid JSON payload.'}, status=400)
